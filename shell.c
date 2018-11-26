c20387e8bc6ddf30f61a8f9b0eee0d75775604ca
Zer0-M <mjamil90@stuy.edu>
2018-11-26 18:12:49 -0500

    basic redirection working partially, but requires more testing

--- shell.c
+++ shell.c
@@ -43,16 +43,40 @@ int lenarray(char*** arr){
 	}
 	return i;
 }
+int lenarrayl(char** arr){
+	int i=0;
+	while(arr[i]){
+		i++;
+	}
+	return i;
+}
 int main(int argc, char *argv[]){
   int* status=malloc(sizeof(int));
-    while(WEXITSTATUS(*status)!=5){
+  *status=0;
+  int backup_stdout=dup(STDOUT_FILENO);
+  int backup_stdin=dup(STDIN_FILENO);
+    while(WIFEXITED(*status)){
       char * str1=readline("shell$ ");
       char *** args=parse_multiple(str1);
       for(int i=0;i<lenarray(args);i++){
 			if(!fork()){
+				int executed=1;
+				for(int j=0;j<lenarrayl(args[i]);j++){
+					if(!strcmp(args[i][j],">")){
+						int fd=open(args[i][j+1],O_WRONLY|O_CREAT);
+						int new=dup2(fd,STDOUT_FILENO);
+						execlp(args[i][j-1],args[i][j-1],NULL);
+					}
+					else if(!strcmp(args[i][j],"<")){
+						int fd=open(args[i][j+1],O_RDONLY);
+						int new=dup2(fd,STDIN_FILENO);
+						execlp(args[i][j-1],args[i][j-1],NULL);
+					}
+				}
 				execvp(args[i][0],args[i]);
-				exit(1);
-		}
+				dup2(backup_stdout,STDOUT_FILENO);
+				dup2(backup_stdin,STDIN_FILENO);
+			}
 			else{
 				wait(status);
 			}
@@ -60,7 +84,7 @@ int main(int argc, char *argv[]){
 		if(!strcmp(args[0][0],"exit")){
 			free(status);
 			free(str1);
-			exit(5);
+			exit(1);
 		}
 		if(!strcmp(args[0][0],"cd")){
 			chdir(args[0][1]);
